184455bb9c2bbf4683db86f4074b0e53c7ab90e3
Emmanuel Onung <emyonung@fb.com>
2022-06-20 16:55:39 -0700

    Hid keys

--- app/src/main/java/com/example/studentarena/ParseApplication.java
+++ app/src/main/java/com/example/studentarena/ParseApplication.java
@@ -12,8 +12,8 @@ public class ParseApplication extends Application {
         super.onCreate();
 
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("WO7pgZgM1VlakaqvPjvF6oPnv5hdOhJWlwrea52u")
-                .clientKey("6u58gIom9F6TUKd74yTnwDFe1GqRLBOzyvFm8QYV")
-                .server("https://parseapi.back4app.com").build());
+                .applicationId(getString(R.string.application_id))
+                .clientKey(getString(R.string.client_key_id))
+                .server(getString(R.string.server)).build());
     }
 }
