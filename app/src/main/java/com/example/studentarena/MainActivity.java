e7584e9461b3df7160f8b545fcb577df1a58d6ef
Emmanuel Onung <emyonung@fb.com>
2022-06-21 16:51:59 -0700

    Created compose, feed, message, profile fragments. Edited the bottom navigation to include message. Added some icon png.

--- app/src/main/java/com/example/studentarena/MainActivity.java
+++ app/src/main/java/com/example/studentarena/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.studentarena;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.studentarena.fragments.ComposeFragment;
+import com.example.studentarena.fragments.FeedFragment;
+import com.example.studentarena.fragments.MessageFragment;
+import com.example.studentarena.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragmentToShow = null;
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        fragmentToShow = new FeedFragment();
+                        break;
+                    case R.id.action_compose:
+                        fragmentToShow = new ComposeFragment();
+                        break;
+                    case R.id.action_message:
+                        fragmentToShow = new MessageFragment();
+                        break;
+                    case R.id.action_profile:
+                        fragmentToShow = new ProfileFragment();
+                        break;
+                    default: break;
+                }
+                if (fragmentToShow != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.flContainer, fragmentToShow).commit();
+                }
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 }
